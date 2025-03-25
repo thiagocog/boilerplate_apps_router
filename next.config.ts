bbfc531eebe06c0f68e7b8b2ea11521285f049d7
Thiago C. de O. Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2025-03-25 19:39:16 +0000

    trying husky config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  swcMinify: false // Disable SWC minification
 };
 
 export default nextConfig;
