862d0443da986334ae6e4a581fb6292da6d3a3b3
Thiago C. de O. Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2025-03-31 22:46:57 +0100

    foo4

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Lint & Prettify TS and JS files
-  '*.{js, jsx, ts, tsx}': (filenames) => [
+  '*.{js,jsx,ts,tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `npm run lint --fix . ${filenames.join(' --file')}`,
     `npm test -- --findRelatedTests ${filenames.join(' ')}`
