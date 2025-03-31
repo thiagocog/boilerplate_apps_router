20ac02cad69948ad7b19c64868a952ac16b80947
Thiago C. de O. Gonçalves <thiago.cgoncalves@al.infnet.edu.br>
2025-03-31 22:37:03 +0100

    minor .lintstagedrc fix

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,6 +3,6 @@ module.exports = {
   '*.{js, jsx, ts, tsx}': (filenames) => [
     `prettier --write ${filenames.join(' ')}`,
     `npm run lint --fix . ${filenames.join(' --file')}`,
-    `npm run test -- --findRelatedTests ${filenames.join(' ')}`
+    `npm test -- --findRelatedTests ${filenames.join(' ')}`
   ]
 };
